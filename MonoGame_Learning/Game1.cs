0bc8e4be6b419876f82c605b237c2457b0a808de
sebas <sebastian_123_3@hotmail.com>
2020-10-03 11:39:23 -0500

    :tada: Initial Commit

---
+++ MonoGame_Learning/Game1.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace MonoGame_Learning
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+        private SoundEffect mySound;
+        private SoundEffectInstance soundEffectInstance;
+        private Song bgSong;
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+            mySound = Content.Load<SoundEffect>("Audio/Music/awesomeness");
+            bgSong = Content.Load<Song>("Audio/Music/pianoBg");
+
+            // TODO: use this.Content to load your game content here
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+            // soundEffectInstance = mySound.CreateInstance();
+            // soundEffectInstance.Volume = .25f;
+            // soundEffectInstance.Play();
+            MediaPlayer.IsRepeating = true;
+            MediaPlayer.Volume = .25f;
+            MediaPlayer.Play(bgSong);
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+
+            base.Draw(gameTime);
+        }
+    }
+}
